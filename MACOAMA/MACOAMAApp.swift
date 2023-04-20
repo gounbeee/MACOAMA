7973f58b1d88afa1e42fd776cc280f47b08a4bb9
gounbeee <gounbeee@gmail.com>
2023-04-20 21:15:40 +0900

    Initial Commit

---
+++ MACOAMA/MACOAMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MACOAMAApp.swift
+//  MACOAMA
+//
+//  Created by Si Young Choi on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct MACOAMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
